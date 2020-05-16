b38d7d1aa180f3a4ea3b5d81fbc3fcc61b645d3b
Luis Quispe <62967265+lquispeAvatar@users.noreply.github.com>
2020-05-16 13:47:35 -0500

    Añadiendo funcionalidad de return top

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 //Return top
-const $btnReturn = document.querySelector("#returnTop");
-$btnReturn.addEventListener("click", () => {
-  window.scroll({
-    top: 100 + "%",
-    behavior: "smooth",
+
+window.addEventListener("scroll", () => {
+  const $btnReturn = document.querySelector("#returnTop");
+  let y = window.scrollY;
+
+  if (y > 100) {
+    $btnReturn.classList.add("is-active");
+  } else {
+    $btnReturn.classList.remove("is-active");
+  }
+
+  $btnReturn.addEventListener("click", () => {
+    window.scroll({
+      top: 100 + "%",
+      behavior: "smooth",
+    });
   });
 });
